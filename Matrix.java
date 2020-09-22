46eac8e8d889107866894aaa30408ce6e9489f11
basaratojo <pierrepaul.charbonnier@gmail.com>
2020-09-22 18:35:57 +0200

    comment

--- Matrix.java
+++ Matrix.java
@@ -34,12 +34,18 @@ public class Matrix {
         printer(callFM1);
         System.out.println("The second matrix is : ");
         printer(callFM2);
-
+        /*
+         * We need this condition to check that the column of the first matrix is not
+         * equal to the row of the second matrix.
+         */
         if(callFM1[0].length!= callFM2.length) {
             System.out.println("this multiplication is not possible");
             return ;
         }
-
+        /*
+         * We call the method that multiply the two matrix and then we print the result .
+         * (In this case ,I'm using the "if" to first check if it is a multiplication of vector and than a matrix multiplication. )
+         */
         if (callFM1.length == 1 && callFM2[0].length == 1) {
             System.out.println("The result of the multiplication is equal to  ");
             int r = vectorMultiplication(callFM1,callFM2) ;
@@ -54,6 +60,9 @@ public class Matrix {
             printer(result);
         }
     }
+    /*
+     * This method is used to fill the number that you choose in the matrix using a loop.
+     */
     public static int[][] fillMatrix1 (int r1,int c1) {
 
         int[][] matrix1 = new int[r1][c1];
@@ -64,22 +73,21 @@ public class Matrix {
             for(int j=0; j<c1; j++) {
                 System.out.println("Enter element "+ i + " ," + j);
                 matrix1[i][j] = scan.nextInt();
-
-
-
-
             }
         }
         return matrix1;
     }
+    /*
+     * This method is used to print the matrix.
+     */
     public static void printer(int mat[][]) {
         for(int i =0; i<mat.length; i++)
             System.out.println(Arrays.toString( mat[i]));
-
         System.out.println();
-
     }
-
+    /*
+     * This method is used to multiply two vectors with 3 "for ".
+     */
     public static int vectorMultiplication (int[][] m1, int[][] m2) {
         int c = 0;
         for(int i=0; i<m1.length; i++) {
@@ -87,33 +95,25 @@ public class Matrix {
                 for(int k=0; k<m1[0].length ; k++)
                 {
                     c += m1[i][k] * m2[k][j];
-
-
                 }
             }
-
         }
         return c;
-
     }
-
-
+    /*
+     * This method is used to multiply two matrix with 3 "for " to make the multiplication working well.
+     */
     public static int[][] multiplication (int[][] m1,int[][] m2)
     {
         int c[][]= new int [m1.length][m2[0].length];
-        System.out.println("row m2[1].length "+ m2[0].length);
-        System.out.println("column m1.length "+ m1.length);
         for(int i=0; i<m1.length; i++) {
             for(int j=0; j<m2[0].length; j++) {
                 c[i][j] = 0;
                 for(int k=0; k<m1[0].length ; k++)
                 {
                     c[i][j] += m1[i][k] * m2[k][j];
-
-
                 }
             }
-
         }
         return c ;
     }
